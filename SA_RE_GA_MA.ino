20698db26800b3e6ea7aadedc152855ce0d0c9e4
vanshikabordia <85024087+vanshikabordia@users.noreply.github.com>
2021-07-11 23:20:56 +0530

    Add files via upload

---
+++ SA_RE_GA_MA.ino
@@ -0,0 +1,43 @@
+int buzzer=13;
+
+void setup()
+{
+  pinMode(13,OUTPUT);
+}
+
+void loop()
+{
+  tone(13,240);
+  delay(400);
+    noTone(13);
+  delay(400);
+  tone(13,270);
+  delay(400);
+    noTone(13);
+  delay(400);
+  tone(13,300);
+  delay(400);
+    noTone(13);
+  delay(400);
+  tone(13,320);
+  delay(400);
+    noTone(13);
+  delay(400);
+  tone(13,360);
+  delay(400);
+    noTone(13);
+  delay(400);
+  tone(13,400);
+  delay(400);
+    noTone(13);
+  delay(400);
+  tone(13,450);
+  delay(400);
+    noTone(13);
+  delay(400);
+  tone(13,480);
+  delay(400);
+    noTone(13);
+  delay(400);
+ }
+ 
